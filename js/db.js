15101b100fdfbd9adbdd5fb0ff293f4e8111ed2d
Marcos Á. Medina <marcmedi1@hotmail.com>
2021-06-09 22:48:10 -0300

    add first files. Regist view done

---
+++ js/db.js
@@ -0,0 +1,54 @@
+
+// PROFESOR 
+// {
+// 	nombre: string, 
+// 	usuario: string, 
+// 	password: string, 
+// 	profesor: stringID > profesorList, 
+// 	nivel: string
+// }
+
+const profesorList = [];
+
+const profesorListTest = [
+	{
+		nombre: "Santi Fig",
+		usuario: "santFig",
+	},
+	{
+		nombre: "Santiago Fig",
+		usuario: "santFig2",
+	}
+];
+
+// ALUMNO 
+// {
+// 	nombre: string, 
+// 	usuario: string, 
+// 	password: string, 
+// 	profesor: stringID > profesorList, 
+// 	nivel: string,
+// 	entregas: [ejercicioObj],
+// 	promedio: string,
+// 	nivel: string,
+// }
+const alumnoList = [];
+
+const alumnoListTest = [
+	{nombre: "Marcos Medina", usuario: "marcmed", password: "mkmPass21", profesor: "santifag", nivel: "inicial"},
+	{nombre: "Emanuel Díaz", usuario: "emadiaz", password: "emaPass12", profesor: "santifag", nivel: "inicial"},
+	{nombre: "Cristian Poggi", usuario: "crispog", password: "crisPass4", profesor: "santifag", nivel: "inicial"},
+];
+
+// EJERCICIO 
+// {
+// 	titulo: string, 
+// 	usuario: string, 
+// 	password: string, 
+// 	profesor: stringID > profesorList, 
+// 	nivel: string
+// }
+
+const ejerciciosList = [];
+
+const ejerciciosListTest = [];
---
+++ js/registro.js
@@ -0,0 +1,1 @@
+console.log("ALUMNOS TEST", alumnoListTest)

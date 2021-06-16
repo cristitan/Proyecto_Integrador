1fc7dd1e19293c747bc903340611fd36d6fe4059
Marcos Á. Medina <marcmedi1@hotmail.com>
2021-06-15 21:56:06 -0300

    add mostarAlert function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
-function registrarDocente() { // WIP Validaciones
-	let nombreDocente = document.querySelector("#profesor_first_name").value;
-	let usuarioDocente = document.querySelector("#profesor_user_name").value;
-	let passwordDocente = document.querySelector("#profesor_password").value;
+function registrarDocente() {
+  // WIP Validaciones
+  let nombreDocente = document.querySelector("#profesor_first_name").value;
+  let usuarioDocente = document.querySelector("#profesor_user_name").value;
+  let passwordDocente = document.querySelector("#profesor_password").value;
 
-	let nuevoDocente = new Docente(
-		nombreDocente,
-		usuarioDocente,
-		passwordDocente
-	);
-	docenteList.push(nuevoDocente); //docenteList es una variable que está en DB
-	// listarDocente()
+  let nuevoDocente = new Docente(
+    nombreDocente,
+    usuarioDocente,
+    passwordDocente
+  );
+  docenteList.push(nuevoDocente); //docenteList es una variable que está en DB
+  // listarDocente()
 }
 function listarDocente() {
-	for (let i = 0; i < docenteList.length; i++) {
-		console.log(docenteList[i])
-	}
+  for (let i = 0; i < docenteList.length; i++) {
+    console.log(docenteList[i]);
+  }
+}
+
+// POR AHORA SOLO ES PARA ERRORES
+function mostrarAlerta(texto) {
+  M.toast({
+    html: `<span><i class="material-icons left red-text">error_outline</i><b>${texto}</b></span>`,
+    displayLength: 4000,
+    class: "toast",
+  });
 }

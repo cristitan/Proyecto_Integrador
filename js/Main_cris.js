2fa5c44a5dcd58fb0b6a375fd104d3c12940bfd2
Cristian Poggi <cristian.poggi@globant.com>
2021-06-18 20:46:49 -0300

    Adding logic in db_cris and Main_Cris

---
+++ js/Main_cris.js
@@ -0,0 +1,257 @@
+// POR AHORA SOLO ES PARA ERRORES
+// tipoDeAlerta = "success", "alert", "error"
+function mostrarAlerta(texto, tipoDeAlerta) {
+    const statesStyles = {
+      error: ["red-text", "cancel"],
+      alert: ["yellow-text text-darken-3", "error"],
+      success: ["green-text text-darken-3", "check_circle"],
+    };
+    const classToAssign = `toast toast_${tipoDeAlerta}`;
+    M.toast({
+      html: `<span><i class="material-icons left ${statesStyles[tipoDeAlerta][0]}">${statesStyles[tipoDeAlerta][1]}</i><b>${texto}</b></span>`,
+      displayLength: 5000,
+      classes: classToAssign,
+    });
+  }
+  
+  /*-------------------------------------- VALIDACIONES ----------------------------------------- */
+  
+  function validarNombre(nombre) {
+    const symbolChars = "!`@#$%^&*()+=-[]\\';,./{}|\":<>?~_";
+    if (nombre.length < 4) {
+      mostrarAlerta("El Nombre debe tener al menos 4 caracteres", "error");
+      return false;
+    }
+    for (var i = 0; i < nombre.length; i++) {
+      const letraDeNombre = nombre[i];
+      if (symbolChars.includes(letraDeNombre)) {
+        mostrarAlerta("El Nombre no debe contener símbolos", "error");
+        return false;
+      }
+    }
+    return true;
+  
+  }
+  
+  function validarUsuario(userName, list) {
+    // list es la lista que pase como parámetro, docentesList, alumnosList
+    for (const elemUsuario of list) {
+      // usuario es el elemento guardado e iterado del list
+      // (usuario docente > docentesList | usuario alumno > alumnosList)
+      if (elemUsuario.usuario === userName) {
+        // si el usuario registrado es igual al usuario nuevo, entonces ya existe
+        mostrarAlerta("Ese Usuario ya fue tomado, escriba otro.", "error");
+        return false; // como ya existe, entonces retornamos false
+      }
+    }
+    // en caso de que no se haya encontrado ningun usuario que sea igual al usuario nuevo
+    // entonces retornamos true
+    return true;
+  }
+  
+  // function validarUserDocente(user) {
+  //   for (let i = 0; i < docenteList.length; i++) {
+  //     if (user === docenteList[i].usuario) {
+  //       return false
+  //     }
+  //   }
+  //   return true
+  // }
+  
+  // function validarUserAlumno(user) {
+  //   for (let i = 0; i < alumnoList.length; i++) {
+  //     if (user === alumnoList[i].usuario) {
+  //       return false
+  //     }
+  //   }
+  //   return true
+  // }
+  
+  // btn_RegistrarDocente.addEventListener("click", ()=>{
+  //   // ---
+  //   validarUsuario(inputUser.value, docentesList)
+  // })
+  
+  function chequearMinus(texto) {
+    for (const letra of texto) {
+      if (letra.charCodeAt(0) >= 97 && letra.charCodeAt(0) <= 122) {
+        return true;
+      }
+    }
+    return false;
+  }
+  function chequearMayus(texto) {
+    for (const letra of texto) {
+      if (letra.charCodeAt(0) >= 65 && letra.charCodeAt(0) <= 90) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function chequearNumero(textoAValidar) {
+    const numeros = "0123456789";
+    for (const numero of numeros) {
+      if (textoAValidar.includes(numero)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function validarPass(password) {
+    const tieneLargo = password.length > 4;
+    // console.log("tieneLargo > ", tieneLargo);
+  
+    const tieneMinuscula = chequearMinus(password);
+    // console.log("tieneMinuscula > ", tieneMinuscula);
+  
+    const tieneMayuscula = chequearMayus(password);
+    // console.log("tieneMayuscula > ", tieneMayuscula);
+  
+    const tieneNumero = chequearNumero(password);
+    // console.log("tieneNumero > ", tieneNumero);
+  
+    if (tieneLargo && tieneMinuscula && tieneMayuscula && tieneNumero) {
+      return true;
+    } else {
+      mostrarAlerta(
+        `Contraseña inválida. Corrija: ${
+          !tieneLargo && "cantidad de caracteres,"
+        } ${!tieneMinuscula && "al menos una minúscula,"} ${
+          !tieneMayuscula && "al menos una mayuscula,"
+        } ${!tieneNumero && "al menos un número,"}`,
+        "error"
+      );
+      return false;
+    }
+  }
+  
+  /* ------------------------------------------ REGISTROS --------------------------------------- */
+  
+  /* ------------------------------------------  Docente  -------------------------------------- */
+  
+  // MOVER REGISTRO A DB PARA TENER CRUD?
+  
+  function registrarDocente() {
+    const nombreDocente = document.querySelector(
+      "input#docente_first_name"
+    ).value;
+    const usuarioDocente = document.querySelector(
+      "input#docente_user_name"
+    ).value;
+    const passwordDocente = document.querySelector(
+      "input#docente_password"
+    ).value;
+    const nuevoDocente = new Docente(
+      nombreDocente,
+      usuarioDocente,
+      passwordDocente
+    );
+  
+    const validadorPushNombre = validarNombre(nombreDocente);
+    const validadorPushUser = validarUsuario(usuarioDocente, docentesList);
+    const validadorPushPass = validarPass(passwordDocente);
+  
+    if (validadorPushNombre && validadorPushUser && validadorPushPass) {
+      docenteList.push(nuevoDocente);
+      listarDocente();
+      mostrarAlerta("Se ha registrado correctamente!", "success");
+    } else {
+      mostrarAlerta("Algo salió mal :(", "error");
+    }
+  }
+  
+  document
+    .querySelector("#btnRegistrarDocente")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      registrarDocente();
+    });
+  
+    /* ------------------------------------------  Tarea  -------------------------------------- */
+  
+    //WIP WIP WIP
+  
+    function registrarTarea() {
+      const nombreDocente = document.querySelector(
+        "input#alumno_first_name"
+      ).value;
+      const usuarioDocente = document.querySelector(
+        "input#alumno_user_name"
+      ).value;
+      const passwordDocente = document.querySelector(
+        "input#alumno_password"
+      ).value;
+      const nuevoDocente = new Docente(
+        nombreAlumno,
+        usuarioAlumno,
+        passwordAlumno
+      );
+    
+      const validadorPushNombre = validarNombre(nombreAlumno);
+      const validadorPushUser = validarUsuario(usuarioAlumno, alumnoList);
+      const validadorPushPass = validarPass(passwordAlumno);
+    
+      if (validadorPushNombre && validadorPushUser && validadorPushPass) {
+        alumnoList.push(nuevoAlumno);
+        listarDocente();
+        mostrarAlerta("Se ha registrado correctamente!", "success");
+      } else {
+        mostrarAlerta("Algo salió mal :(", "error");
+      }
+    }
+    
+    document
+      .querySelector("#btnRegistrarDocente")
+      .addEventListener("click", (e) => {
+        e.preventDefault();
+        registrarDocente();
+      });
+  
+  /* ------------------------------------------  Alumno  -------------------------------------- */
+  
+  
+  function registrarAlumno() {
+  
+    // WIP = WORK IN PROGRESS
+  
+    let nombre = document.querySelector("#alumno_first_name").value;
+    let usuario = document.querySelector("#alumno_user_name").value;
+    let password = document.querySelector("#alumno_password").value;
+    // AGREGAR NIVEL let password = document.querySelector("#").value;
+    // Tomar valor del select
+  
+    let nuevoAlumno = new Alumno(nombre, usuario, password);
+  
+    let validadorPushNombre = validarNombre(nombre);
+    let validadorPushUser = validarUserAlumno(usuario);
+    let validadorPushPass = validarPass(password);
+  
+    if (validadorPushNombre && validadorPushUser && validadorPushPass) {
+      docenteList.push(nuevoDocente);
+      listarDocentees();
+    } else if (!validadorPushNombre) {
+      mostrarAlerta(
+        "Error:  Nombre incorrecto. Por favor, ingrese un nombre sin símbolos. ",
+        "error"
+      );
+    } else if (!validadorPushUser) {
+      mostrarAlerta(
+        "Error, ya existe este usuario, elija otro nombre de usuario. ",
+        "error"
+      );
+    } else if (!validadorPushPass) {
+      mostrarAlerta(
+        "Error, su contraseña no cumple con los requisitos. Asegúrese de que tenga al menos 4 carácteres, una minúscula, una mayúscula y un número. ",
+        "error"
+      );
+    }
+  }
+  
+  function listarAlumnos() {
+    for (let i = 0; i < alumnoList.length; i++) {
+      console.log(alumnoList[i]);
+    }
+  }
+  
--- js/db_Cris.js
+++ js/db_Cris.js
@@ -1,249 +1,236 @@
-// ------------------------------------------- Docente  --------------------------------------------
-
-const profesorList = [];
-
-// Docente 
-// {
-// 	nombre: string, 
-// 	usuario: string, 
-// 	password: string, 
-// 	profesor: stringID > profesorList, 
-// 	nivel: string
-// }
-
 class Docente {
   constructor(unNombre, unUsuario, password) {
     this.nombre = unNombre;
     this.usuario = unUsuario;
     this.password = password;
-    this.alumnos = [];
-    this.tareas = [];
-    this.id = profesorList.length; // asigna un numero incremental y unico
-    //this.id = Date.now(); // asigna un numero incremental y unico
+    this.alumnosAsignados = [];
+    this.tareasPlanteadas = [];
+  }
+}
+
+ class Alumno {
+  constructor(nombre, usuario, password, usuarioDocenteAsignado) {
+    this.usuario = usuario;
+    this.nombre = nombre;
+    this.password = password;
+    this.usuarioDocenteAsignado = usuarioDocenteAsignado;
+    this.nivelAlumno = "inicial";
+    this.entregas = [];
+  }
+}
+
+ class Entrega {
+  constructor(usuarioAlumno, id_tarea, audio, devoluciones, nota_final, comentario, estado) {
+    this.usuarioAlumno = usuarioAlumno;
+    this.id_tarea = id_tarea;
+    this.audio = audio;
+    this.devoluciones = devoluciones;
+    this.nota_final = nota_final;
+    this.comentario = comentario;
+    this.estado = estado;
+  }
+}
+
+ class Tarea {
+  constructor(unaTarea, unTitulo, unaDescripcion, unNivel, unaNota, unaImagen) {
+    this.idTarea = unaTarea;
+    this.titulo = unTitulo;
+    this.descripcion = unaDescripcion;
+    this.nivel = unNivel;
+    this.notaMax = unaNota;
+    this.imagen = unaImagen;
+    this.entregas = [];
   }
 }
 
 
-document
-  .querySelector("#btnRegistrarDocente")
-  .addEventListener("click", registrarProfesor);
-
-
-function registrarProfesor() {
-  let nombreProfe = document.querySelector("#profesor_first_name").value;
-  let usuarioProfe = document.querySelector("#profesor_user_name").value;
-  let passwordProfe = document.querySelector("#profesor_password").value;
-
-  let nuevoProfesor = new Docente(
-    nombreProfe,
-    usuarioProfe,
-    passwordProfe
-  );
-
-  let validadorPushNombre = validarNombre(nombreProfe)
-  let validadorPushUser = validarUserDocente(usuarioProfe)
-  let validadorPushPass = validarPass(passwordProfe)
-
-  if (validadorPushNombre && validadorPushUser && validadorPushPass) {
-
-    profesorList.push(nuevoProfesor);
-    listarProfesores()
-
-  } else if (!validadorPushNombre) { 
-    alert ("Error:  Nombre incorrecto. Por favor, ingrese un nombre sin símbolos. ")
-    }
-    
-    else if (!validadorPushUser) { 
-    alert ("Error, ya existe este usuario, elija otro nombre de usuario")
-    }
-    
-    else if (!validadorPushPass) { 
-    alert ("Error, su contraseña no cumple con los requisitos. Asegúrese de que tenga al menos 4 carácteres, una minúscula, una mayúscula y un número")
-    }
-}
-
-//Para listar Profes/Docentes
-
-function listarProfesores() {
-  for (let i = 0; i < profesorList.length; i++) {
-    console.log(profesorList[i])
-  }
-}
-
-// WIP = WORK IN PROGRESS
-// function crearTarea(profesorUsr, titulo, descripion, img, notaMax, nivel) { // SOLO PROFESOR
-//   const nuevaTarea = {
-//     idTarea: profesorUsr.tareasPlanteadas.length + 1,
-//     titulo, // DESTRUCTURING OBJ
-//     descripion,
-//     img,
-//     notaMax,
-//     nivel,
-//     entregas: [] // inicializa vacío
+// class Tarea_Planteada {
+//   constructor(id, titulo, descripcion, imagen) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.descripcion = descripcion;
+//     this.imagen = imagen;
 //   }
-//   profesorUsr.tareasPlanteadas.push(nuevaTarea)
 // }
 
+const usuarioLogeado = {
+  // REFERENCIAS
+  listaPerteneciente: [],
+  indice: 0, //
+  usuarioId: "", // ? el usuario id para enviar como parametro a getAlumno para encontrar el alumno
+}; // Por qué no poner dataUsuarioLogeado dentro del usuarioLogeado
+const dataUsuarioLogeado =
+  usuarioLogeado.listaPerteneciente[usuarioLogeado.indice];
+// usuarioLogeado.listaPerteneciente = alumnosList
 
-// ------------------------------------------- ALUMNO  --------------------------------------------
+
+ const alumnosList = [];
 
 
 
+ const docentesList = [];
 
-// Alumno creación de objeto y funciones
 
-const alumnoList = [];
-
-class Alumno {
-  constructor(unNombre, unUsuario, password, unNivel) {
-    this.nombre = unNombre;
-    this.usuario = unUsuario;
-    this.password = password;
-    this.nivelAlumno = "inicial"
-    this.docenteId = null; //es un valor que está declarado pero es nulo, no undefined
-    this.tareas = [];
-    this.id = alumnoList.length; // asigna un numero incremental desde 0
-  }
+// // WIP = WORK IN PROGRESS
+function crearTarea(profesorUsr, titulo, descripion, img, notaMax, nivel) {
+  // SOLO PROFESOR
+  const nuevaTarea = {
+    idTarea: profesorUsr.tareasPlanteadas.length,
+    titulo, // DESTRUCTURING OBJ
+    descripion,
+    img,
+    notaMax,
+    nivel,
+    entregas: [], // inicializa vacío
+  };
+  profesorUsr.tareasPlanteadas.push(nuevaTarea);
 }
 
-let userLogeado = "user loggeado"
+// // WIP = WORK IN PROGRESS
+// function realizarEntrega(audio, comentario) {
+//   // SOLO ALUMNO
+//   const nuevaEntrega = {
+//     userAlumno: userLogeado.user,
+//     idTarea: idTarea,
+//     password: password,
+//     alumnos: [],
+//     tareasPlanteadas: [],
+//   };
+//   getAlumno(userLogeado.user).entregas.push(nuevaEntrega);
+//   getTarea(getProfesor(userLogeado.profesor), idTarea).entregas.push(
+//     nuevaEntrega
+//   );
+// }
 
-// Lógica para mostrar y ocultar secciones
-// document.querySelector("wsf").addClass("hidden"); // Todos
-// document.querySelector("wsf").removeClass("hidden") // Para mostrar específico
+// const docentesListTest = [
+//   {
+//     nombre: "Santi Fig",
+//     usuario: "santFig",
+//     password: "myPass123",
+//     alumnos: [
+//       {
+//         nombre: "Marcos Medina",
+//         usuario: "marcmed",
+//         password: "mkmPass21",
+//         profesor: "santifag",
+//         nivel: "inicial",
+//       },
+//       {
+//         nombre: "Emanuel Díaz",
+//         usuario: "emadiaz",
+//         password: "emaPass12",
+//         profesor: "santifag",
+//         nivel: "inicial",
+//       },
+//       {
+//         nombre: "Cristian Poggi",
+//         usuario: "crispog",
+//         password: "crisPass4",
+//         profesor: "santifag",
+//         nivel: "inicial",
+//       },
+//     ],
+//     tareas: [
+//       // TAREAS PLANTEADAS POR EL DOCENTE
+//       {
+//         id: 12,
+//         title: "Tareas de prueba",
+//         descripion: "Descripcion larga asfhqawfi",
+//         imagen: "url/here.dom",
+//         nota: 100, // nota máxima de tarea
+//         nivel: "inicial", // "inicial"/"intermedio"/"avanzado"
+//         entregas: [
+//           {
+//             alumno: "emadiaz",
+//             audio: "ulr/",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     nombre: "Santiago Fig",
+//     usuario: "santFig2",
+//   },
+// ];
+// const alumnosListTest = [
+//   {
+//     nombre: "Marcos Medina",
+//     usuario: "marcmed",
+//     password: "mkmPass21",
+//     docente: "santifag",
+//     nivel: "inicial",
+//     entregas: [],
+//   },
+//   {
+//     nombre: "Emanuel Díaz",
+//     usuario: "emadiaz",
+//     password: "emaPass12",
+//     profesor: "santifag",
+//     nivel: "inicial",
+//   },
+//   {
+//     nombre: "Cristian Poggi",
+//     usuario: "crispog",
+//     password: "crisPass4",
+//     profesor: "santifag",
+//     nivel: "inicial",
+//     entregas: [],
+//   },
+// ];
 
+function getDocente(usuarioId) {
+  return docentesList.some((docente) => docente.usuario === usuarioId);
+  // Some retorna true si hay un docente que cumpla ese id
 
-// document
-// .querySelector("#btnRegistrarAlumno")
-// .addEventListener("click", registrarAlumno);
+  // Busca un docente, cuyo usuario sea usuario id.
+  // Si no existe da undefined si existe te da el docente return docentesList.find((docente) => docente.usuario === usuarioId); 
 
-function registrarAlumno() { // WIP = WORK IN PROGRESS
-  
-  let nombre = document.querySelector("#alumno_first_name").value;
-  let usuario = document.querySelector("#alumno_user_name").value;
-  let password = document.querySelector("#alumno_password").value;
-  // AGREGAR NIVEL let password = document.querySelector("#").value;
-  // Tomar valor del select
-  
-  let nuevoAlumno = new Alumno(
-    nombre,
-    usuario,
-    password,
-    );
-    
-    let validadorPushNombre = validarNombre(nombre)
-    let validadorPushUser = validarUserAlumno(usuario)
-    let validadorPushPass = validarPass(password)
-    
-    if (validadorPushNombre && validadorPushUser && validadorPushPass) {
-      
-      profesorList.push(nuevoProfesor);
-      listarProfesores()
-      
-    } else if (!validadorPushNombre) { 
-      alert ("Error:  Nombre incorrecto. Por favor, ingrese un nombre sin símbolos. ")
-      }
-      
-      else if (!validadorPushUser) { 
-      alert ("Error, ya existe este usuario, elija otro nombre de usuario")
-      }
-      
-      else if (!validadorPushPass) { 
-      alert ("Error, su contraseña no cumple con los requisitos. Asegúrese de que tenga al menos 4 carácteres, una minúscula, una mayúscula y un número")
-      }
-    
-  }
-  
-  function listarAlumnos() {
-    for (let i = 0; i < alumnoList.length; i++) {
-      console.log(alumnoList[i])
-    }
-  }
-  
-  // WIP = WORK IN PROGRESS
-  // function realizarEntrega(audio, comentario) { // SOLO ALUMNO
-  //   const nuevaEntrega = {
-  //     userAlumno: userLogeado.user,
-  //     idTarea: idTarea,
-  //     password: password,
-  //     // alumnos: [],
-  //     // tareasPlanteadas: []
+  // for (const docente of docentesList) {
+  //   if (docente.usuario === usuarioId) {
+  //     return docente;
   //   }
-  //   getAlumno(userLogeado.user).entregas.push(nuevaEntrega);
-  //   getTarea(getProfesor(userLogeado.profesor), idTarea).entregas.push(nuevaEntrega);
   // }
-  
-  // ------------------------------------------- VALIDACIONES --------------------------------------------
-  
-  
-  
-  function validarNombre(nombre) {
-    
-    // const nombreProfe No tenga simbolos y no esté vacío
-    
-    let symbolChars = "!`@#$%^&*()+=-[]\\\';,./{}|\":<>?~_";   
-    let validarSuimbolosUsuario = true
-
-  for (var i = 0; i < nombre.length; i++)
-
-    {      
-        if (symbolChars.indexOf(nombre.charAt(i)) != -1){    
-
-        return validarSuimbolosUsuario = false
-
-        } 
-    }
-
-  const nombreEsValidado = nombre === ""
-
-  return nombreEsValidado && validarSuimbolosUsuario
 }
 
-
-function validarUserDocente(user) {
-
-  //ALUMNO Y PROFE
-
-  for (let i = 0; i < profesorList.length; i++) {
-
-    if (user === profesorList[i].usuario) {
-      return false
+function getAlumno(usuarioId) {
+  for (const alumno of alumnosList) {
+    if (docente.usuario === usuarioId) {
+      return alumno;
     }
   }
- return true
-
 }
 
-function validarUserAlumno(user) {
+// const listaDeTareasTotalesQueTieneElAlumno = [];
+// const listaDeTareasPendientesQueTieneElAlumno = [];
+// const listaDeTareasRealizadasQueTieneElAlumno = [];
 
-  //ALUMNO Y PROFE
+// const docenteDelAlumnoLogeado = getDocente(
+//   dataUsuarioLogeado.usuarioDocenteAsignado
+// );
+// // OBTENER TODAS LAS TAREAS PLANTEADAS POR EL DOCENTE SEGUN NIVEL PARA EL ALUMNO
+// for (const tareaPlanteada of docenteDelAlumnoLogeado.tareasPlanteadas) {
+//   if (tareaPlanteada.nivel === usuarioLogeado.nivel) {
+//     //usuarioLogueado es un alumno
+//     listaDeTareasTotalesQueTieneElAlumno.push(tareaPlanteada);
+//   }
+// }
 
-  for (let i = 0; i < alumnoList.length; i++) {
+// // OBTENER TODAS LAS TAREAS PLANTEADAS POR EL DOCENTE YA RESUELTAS POR EL ALUMNO
+// for (const tarea of listaDeTareasTotalesQueTieneElAlumno) {
+//   //Cómo llego al profesor
+//   let existe = false;
+//   for (const entrega of dataUsuarioLogeado.entregas) {
+//     if (tarea.id === entrega.idTarea) {
+//       existe = true;
+//       break;
+//       // listaDeTareasRealizadasQueTieneElAlumno.push(tarea)
+//     }
+//   }
+//   if (existe) {
+//     listaDeTareasRealizadasQueTieneElAlumno.push(tarea);
+//   } else {
+//     listaDeTareasPendientesQueTieneElAlumno.push(tarea);
+//   }
+// }
 
-    if (user === alumnoList[i].usuario) {
-      return false
-    }
-  }
- return true
-
-}
-
-
-function validarPass(password) {
-
-  // contraseña alfanumerica mayos a 4 chars y case sensitive
-
-  const validatorLargo = password.length > 4
-  const validatorVacio = password === ""
-  const validarMinusculas = password.toLowerCase() != password
-  const validarMayusculas = password.toUpperCase() != password
-
-  return validatorLargo && validatorVacio && validarMinusculas && validarMayusculas
-
-}
-
-
-
-const ejerciciosList = [];
-
-const ejerciciosListTest = [];

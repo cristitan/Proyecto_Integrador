0f100b7327aa2968a1cc969c46e658942e55cec0
Marcos Angel Medina <marcmedi1@hotmail.com>
2021-06-23 20:04:20 -0300

    move script from index to main.js

--- js/main.js
+++ js/main.js
@@ -335,7 +335,7 @@ function listarDocentes() {
   }
 }
 
-function cambiarNivelAlumno(alumnoData, nivel){
+function cambiarNivelAlumno(alumnoData, nivel) {
   const alumno = getAlumno(alumnoData.usuario);
   alumnoData.nivelAlumno = nivel;
   alumno.nivelAlumno = nivel;
@@ -434,63 +434,71 @@ function selectDocentes() {
 }
 //Funcion para desplegar los docentes en el select
 function selectReporteAlumnos() {
-  let onLvlChangeHandler
-  let nivelOutputElem
-  const onAlumnoChangeHandler = (e)=>{
+  let onLvlChangeHandler;
+  let nivelOutputElem;
+  const onAlumnoChangeHandler = (e) => {
     const dataAlumno = dataUsuarioLogeado.alumnosAsignados[e.target.value];
-    const nombreOutputElem = document.querySelector("#reporteIndividual_nombre");
+    const nombreOutputElem = document.querySelector(
+      "#reporteIndividual_nombre"
+    );
     nivelOutputElem = document.querySelector("#reporteIndividual_nivel");
-    nivelOutputElem.removeEventListener("change", onLvlChangeHandler)
-    const ejerciciosPlanteadosOutputElem = document.querySelector("#reporteIndividual_tareasPlanteadas");
-    const ejerciciosResueltosOutputElem = document.querySelector("#reporteIndividual_tareasEntregadas");
+    nivelOutputElem.removeEventListener("change", onLvlChangeHandler);
+    const ejerciciosPlanteadosOutputElem = document.querySelector(
+      "#reporteIndividual_tareasPlanteadas"
+    );
+    const ejerciciosResueltosOutputElem = document.querySelector(
+      "#reporteIndividual_tareasEntregadas"
+    );
     const cambiarNivelBtn = document.querySelector("#cambiarNivel");
-    let lvlSelectInstance = M.FormSelect.init(nivelOutputElem)
+    let lvlSelectInstance = M.FormSelect.init(nivelOutputElem);
     // funcion que se ejecuta al selecionar un nivel
     onLvlChangeHandler = (e) => {
-      cambiarNivelBtn.removeAttribute("disabled")
-      cambiarNivelBtn.removeEventListener("click", clickBtnHandler)
-      cambiarNivelBtn.addEventListener("click", clickBtnHandler)
-    }
+      cambiarNivelBtn.removeAttribute("disabled");
+      cambiarNivelBtn.removeEventListener("click", clickBtnHandler);
+      cambiarNivelBtn.addEventListener("click", clickBtnHandler);
+    };
 
     // función que reinicia los options del select
-    const habilitarOptions = ()=>{
+    const habilitarOptions = () => {
       nivelOutputElem.innerHTML = `<option value="inicial">Inicial</option>
       <option value="intermedio">Intermedio</option>
       <option value="avanzado">Avanzado</option>`;
       nivelOutputElem.value = dataAlumno.nivelAlumno;
       for (const option of nivelOutputElem.children) {
-        option.setAttribute("disabled", true)
-        if(option.getAttribute("value") === dataAlumno.nivelAlumno){
-          break
+        option.setAttribute("disabled", true);
+        if (option.getAttribute("value") === dataAlumno.nivelAlumno) {
+          break;
         }
       }
-      
+
       lvlSelectInstance = M.FormSelect.init(nivelOutputElem);
-    }
+    };
 
     // función que se ejecuta al presionar el botón de Cambiar Nivel
     const clickBtnHandler = () => {
-      cambiarNivelAlumno(dataAlumno, nivelOutputElem.value)
-      mostrarAlerta(`Se ha cambiado el nivel de <b>${dataAlumno.nombre}</b> a <b>${nivelOutputElem.value}</b>`, "success")
+      cambiarNivelAlumno(dataAlumno, nivelOutputElem.value);
+      mostrarAlerta(
+        `Se ha cambiado el nivel de <b>${dataAlumno.nombre}</b> a <b>${nivelOutputElem.value}</b>`,
+        "success"
+      );
       cambiarNivelBtn.setAttribute("disabled", true);
-      cambiarNivelBtn.removeEventListener("click", clickBtnHandler)
+      cambiarNivelBtn.removeEventListener("click", clickBtnHandler);
       lvlSelectInstance.destroy();
       habilitarOptions();
-    }
-    
-    let ejerciosPlanteadosEspecificos = 0;
+    };
 
+    let ejerciosPlanteadosEspecificos = 0;
 
     // reinicio al botpon para que no tenga funcionalidad y esté deshabilitado
     cambiarNivelBtn.setAttribute("disabled", true);
-    cambiarNivelBtn.removeEventListener("click", clickBtnHandler)
+    cambiarNivelBtn.removeEventListener("click", clickBtnHandler);
 
     // asigno evento de change al select de nivel
-    nivelOutputElem.addEventListener("change", onLvlChangeHandler)
+    nivelOutputElem.addEventListener("change", onLvlChangeHandler);
     // muestro valor del select según el nivel del alumno
-    for (const tarea of dataUsuarioLogeado.tareasPlanteadas){
-      if(tarea.nivel === dataAlumno.nivelAlumno){
-        ejerciosPlanteadosEspecificos++
+    for (const tarea of dataUsuarioLogeado.tareasPlanteadas) {
+      if (tarea.nivel === dataAlumno.nivelAlumno) {
+        ejerciosPlanteadosEspecificos++;
       }
     }
     //reinicio los options para mostrar cuáles están habilitados y cuales no
@@ -498,13 +506,10 @@ function selectReporteAlumnos() {
     nombreOutputElem.innerHTML = dataAlumno.nombre;
     ejerciciosPlanteadosOutputElem.innerHTML = ejerciosPlanteadosEspecificos;
     ejerciciosResueltosOutputElem.innerHTML = dataAlumno.entregas.length;
+  };
 
-  }
-
-  const selectElem = document.querySelector(
-    "select#alumnosAsignados"
-  )
-  selectElem.addEventListener("change", onAlumnoChangeHandler)
+  const selectElem = document.querySelector("select#alumnosAsignados");
+  selectElem.addEventListener("change", onAlumnoChangeHandler);
 
   selectElem.innerHTML = `<option disabled selected>Choose your option</option>`;
   for (let i = 0; i < dataUsuarioLogeado.alumnosAsignados.length; i++) {
@@ -514,8 +519,6 @@ function selectReporteAlumnos() {
 
   M.FormSelect.init(selectElem);
 }
-
-// Funcion para listar los alumnos registrados al momento en la consola.
 
 function listarAlumnos() {
   // for (let i = 0; i < alumnosList.length; i++) {
@@ -571,7 +574,7 @@ function cerrarSesion() {
   usuarioLogeado.listaPerteneciente = [];
   usuarioLogeado.indice = 0;
   usuarioLogeado.usuarioId = "";
-  dataUsuarioLogeado = {}; 
+  dataUsuarioLogeado = {};
   mostrarNavItem("ul#vistaPublico"); // ahora los items van a ser los de navItems para usuarioDesconocido: los botones de inicio y Registro
   mostrarOneSection("section#login"); // Volvemos a la seccion login al cerrar sesion
 }
@@ -582,13 +585,15 @@ document
 
 // ADDEVENTLISTENER
 
-// DASHBOARD DOCENTE > Botón reporte individual 
+// DASHBOARD DOCENTE > Botón reporte individual
 document
-.querySelector("body main section#dashboardDocente button#btnDocenteIrReporteIndividual")
-.addEventListener("click", () => {
-  mostrarOneSection("section#reporteIndividualDocente");
-  selectReporteAlumnos()
-});
+  .querySelector(
+    "body main section#dashboardDocente button#btnDocenteIrReporteIndividual"
+  )
+  .addEventListener("click", () => {
+    mostrarOneSection("section#reporteIndividualDocente");
+    selectReporteAlumnos();
+  });
 
 // Botón logeo Docente
 document
@@ -620,7 +625,6 @@ document
     login(userNameAlumno, passwordAlumno, "alumno");
     mostrarOneSection("section#dashboardAlumno");
   });
-
 
 // Botón Registro Docente
 document
@@ -663,3 +667,51 @@ iterar dataUsuarioLogeado.entregas[i].devolucion
     return totalEntregasCorregidas
 
 */
+ocultarTodo();
+actualizarNavItems();
+mostrarOneSection("section#login")
+// mostrarOneSection("section#tareasPendienteAlumno")
+// mostrarOneSection("section#pantallaPrincipal")
+// mostrarOneSection("section#dashboardAlumno")
+// mostrarOneSection("section#tareasDocente")
+// mostrarOneSection("section#reporteIndividualDocente");
+// mostrarNavItem("ul#usuarioConocido")
+
+document.getElementById("realizarTarea").addEventListener("click", () => {
+  mostrarOneSection("section#realizarEntregaTarea");
+});
+
+document.getElementById("verResueltos").addEventListener("click", () => {
+  mostrarOneSection("section#verEjerciciosResultos");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var selectElem = document.querySelectorAll("select");
+  var tabElemRegistro = document.querySelectorAll("#tabMenuRegistro");
+  var tabElemLogin = document.querySelectorAll("#tabMenuLogin");
+  var tabElemDashboardDocente = document.querySelectorAll(
+    "#tabDashboardDocente"
+  );
+  var tabElemTareasAlumno = document.querySelectorAll("#tabTareasAlumno");
+  let sideBar = M.Sidenav.init(elems);
+
+  let tabRegistro = M.Tabs.init(tabElemRegistro); //, {swipeable: true})
+  let tabLogin = M.Tabs.init(tabElemLogin); //, {swipeable: true})
+
+  let tabDashboardDocente = M.Tabs.init(tabElemDashboardDocente); //, {swipeable: true})
+  let tabTareasAlumno = M.Tabs.init(tabElemTareasAlumno); //, {swipeable: true})
+
+  var elemsModal = document.querySelectorAll(".modal");
+  var instancesModal = M.Modal.init(elemsModal);
+
+  var singleModalElem = document.querySelector("#modalCrearTarea");
+  var instanceModal = M.Modal.getInstance(singleModalElem);
+  const modalbtn = document.querySelector("#crearTareaBtn");
+  modalbtn.addEventListener("click", () => {
+    instanceModal.open();
+    var selectElem = document.querySelectorAll("select#nivel");
+    M.FormSelect.init(selectElem);
+    // Do other stuff
+  });
+});
